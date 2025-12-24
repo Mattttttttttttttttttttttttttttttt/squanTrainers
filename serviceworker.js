0dc944bd8d773ab83d9204be1695d54c331c2815
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2025-12-24 11:39:14 -0800

    makes the website a pwa

---
+++ serviceworker.js
@@ -0,0 +1,48 @@
+// serviceworker.js
+
+const CACHE_NAME = 'squan-trainer-cache-v1';
+const FILES_TO_CACHE = [
+    '/',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Caching files');
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response from cache
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
