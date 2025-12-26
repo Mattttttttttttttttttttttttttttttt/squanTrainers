2dafd063fa389ed81450685ebf1b5c61b28c8782
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2025-12-25 17:35:40 -0800

    update cache files

--- serviceworker.js
+++ serviceworker.js
@@ -1,8 +1,27 @@
 // serviceworker.js
 
-const CACHE_NAME = 'squan-trainer-cache-v1';
+const CACHE_NAME = 'squan-trainer-cache-v1.1.0';
 const FILES_TO_CACHE = [
     '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/404.html',
+    '/caticon.ico',
+    '/caticonbig.png',
+    '/caticonsmall.png',
+    '/serviceworker.js',
+    '/OBLTrainer/index.html',
+    '/OBLTrainer/index.css',
+    '/OBLTrainer/cube.js',
+    '/OBLTrainer/memo_generator.js',
+    '/OBLTrainer/defaultlists.json',
+    '/OBLTrainer/caticon.ico',
+    '/PBLTrainer/index.html',
+    '/PBLTrainer/index.css',
+    '/PBLTrainer/cube.js',
+    '/PBLTrainer/scrambler.js',
+    '/PBLTrainer/generators.json',
+    '/PBLTrainer/favicon.ico',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
 ];
 
