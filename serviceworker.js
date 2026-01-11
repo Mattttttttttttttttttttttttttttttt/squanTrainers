fbf8d53c716468fe53cf5633657343b0394c71c6
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2026-01-10 23:34:01 -0800

    submodule bug fixes

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,7 @@
 
 // hi matt you run this:
 // git submodule update --remote --merge
-const CACHE_NAME = 'squan-trainer-cache-v1.1.6';
+const CACHE_NAME = 'squan-trainer-cache-v1.1.7';
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
